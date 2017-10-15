c8fe20bfc88e4d063c4bca05324e8c4c1bb754ed
David Leigh <daleighan@gmail.com>
2017-10-15 14:48:33 -0700

    Added a field for drawing letters

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import DrawableCanvas from 'react-drawable-canvas'
 
 class Handwriting extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+      isArtist: false,
+      styles: {
+      	backgroundColor: 'white',
+      	width: '64px',
+      	height: '64px'
+      }
+    }
 	}
 
 	render = () => {
 		return (
 			<div>
-				<div>Hello World</div>
+				<div>Sketch a letter!</div>
+				<div className="drawing" style={this.state.styles}> 
+					<DrawableCanvas
+						id="sketch"
+						brushColor="black"
+					/>
+				</div>
+				<div>Get Prediction</div>
 			</div>
 		)
 	}
