The image is now being fed into the classifier, but now the wrong value is being returned

server/MLService.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, abort, jsonify
 from sklearn import datasets, svm, metrics
-from scipy.misc import imread
 import base64
+from scipy.misc import imread, imresize
+
 
 # Load in the digits dataset
 digits = datasets.load_digits()
@@ -11,7 +12,6 @@ data = digits.images.reshape(num_samples, -1)
 classifier = svm.SVC(gamma = 0.001)
 # Fit the model to the numbers data
 classifier.fit(data[:num_samples], digits.target[:num_samples])
-
 
 
 # Instantiate the server
@@ -31,6 +31,11 @@ def predict():
 	filename = 'image.jpg'
 	with open(filename, 'wb') as f:
 		f.write(image_data)
+	to_predict = imread('image.jpg')
+	to_predict = imresize(to_predict, (8, 8))
+	to_predict = to_predict[:, :, 0]
+	predicted = classifier.predict(to_predict.reshape(1, to_predict.shape[0] * to_predict.shape[1]))
+	print(predicted)
 	return jsonify({ 'prediction': 'todo' }), 201
 
 if __name__ == "__main__":