d493dbbe84f6e3d11b80035d467482a63d93dc62
David Leigh <daleighan@gmail.com>
2017-10-16 17:01:41 -0700

    More work done on making MNIST model. fitting is currently running

--- server/MNIST.py
+++ server/MNIST.py
@@ -1,18 +1,28 @@
-import numpy
-import random
-from numpy import arange
 import time
-
-from sklearn import metrics
 from sklearn.datasets import fetch_mldata
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.utils import shuffle
+from sklearn import datasets, svm, metrics
+import pickle
+from sklearn.externals import joblib
 
 
 def run():
-	n_train = 60000
-	n_test = 10000
+	# Load in the digits dataset
 	mnist = fetch_mldata('MNIST original', data_home='./server/MNIST_data')
+	num_samples = len(mnist.data)
+	data = mnist.data.reshape(num_samples, -1)
+	print('Data reshaped')
+	# Create the model
+	# Support Vector Classifaction will be used
+	classifier = svm.SVC(gamma = 0.001)
+	# Fit the model to the numbers data
+	print('Preparing to fit the data')
+	classifier.fit(data[:num_samples], mnist.target[:num_samples])
+	print('Fit complete')
+	#save the modle with pickle
+	joblib.dump(classifier, 'MNIST_PICKLE.pkl')
+	print('pickle dump complete')
+
+	#clf = joblib.load('filename.pkl') use this to load data
 
 
 if __name__ == "__main__":
