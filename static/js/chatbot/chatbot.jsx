cb3fb76add4d53cbd1f42317dc0fb5bc0a67d78d
thismax <max.g.jacobs@gmail.com>
2017-10-20 10:59:18 -0700

    added chatbot skeleton

---
+++ static/js/chatbot/chatbot.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Chatbot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+  
+
+
+  render() {
+    return (
+      <div>
+        hi
+      </div>
+    )
+  }
+}
+
+export default Chatbot;
