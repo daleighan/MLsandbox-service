cea2d38508953ec06aaeac6aa005bb638d49f027
thismax <max.g.jacobs@gmail.com>
2017-10-24 09:49:35 -0700

    added key

--- server/mushrooms/MUSHROOM.py
+++ server/mushrooms/MUSHROOM.py
@@ -1,10 +1,11 @@
 from sklearn.externals import joblib
 from sklearn.cross_validation import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd 
+import numpy as np
 import time
+import json
 
 def run():
     mushroom_data = pd.read_csv("mushroom_data/mushroom_data.csv")
@@ -12,8 +13,15 @@ def run():
     svc = SVC(gamma=.01)
     label_encoder = LabelEncoder()
 
+    key = {}
+
     for col in mushroom_data.columns:
         mushroom_data[col] = label_encoder.fit_transform(mushroom_data[col])
+        mapping = dict(zip(label_encoder.classes_, range(len(label_encoder.classes_))))
+        key[col] = mapping
+
+    with open('key.text', 'w') as file:
+        file.write(json.dumps(key))
 
     mushroom_data.to_csv('mushroom_data/mushroom_data_encoded.csv')
 
