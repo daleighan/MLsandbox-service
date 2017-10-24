c2f41ea45d3af646437d7ee0c077b4358d7ba4c0
David Leigh <daleighan@gmail.com>
2017-10-23 17:35:11 -0700

    Made the training of the model for male voices

--- server/SPEECH/SPEECH_RECOG.py
+++ server/SPEECH/SPEECH_RECOG.py
@@ -5,6 +5,7 @@ from sklearn import preprocessing
 from sklearn.externals import joblib
 import numpy as np
 import time
+import os
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -14,6 +15,28 @@ def get_MFCC(sr, audio):
     features = preprocessing.scale(features)
     return features
 
-    gmm = GMM(n_components = 8, n_iter = 200, convariance_type="diag", n_init=3)
+def run():
+    source = "pygender/train_data/youtube/male"
+    dest = "pygender"
+    files = [os.path.join(source, f) for f in os.listdir(source) if f.endswith('.wav')]
+    
+    features = np.asarray(())
+    
+    for f in files:
+        sr,audio = read(f)
+        vector = get_MFCC(sr, audio)
+        if features.size == 0:
+            features = vector
+        else:
+            features = np.vstack((features, vector))
+    
+    gmm = GMM(n_components = 8, n_iter = 200, covariance_type='diag', n_init=3)
     gmm.fit(features)
+    
+    joblib.dump(gmm,"male.pkl")
 
+if __name__ == "__main__":
+    start_time = time.time()
+    results = run()
+    end_time = time.time()    
+    print("Total Running Time: ", end_time - start_time)
