1304e14bb5ba308cb262b258428df096a21981ae
thismax <max.g.jacobs@gmail.com>
2017-10-23 20:11:23 -0700

    init

---
+++ static/js/mushrooms/options.js
@@ -0,0 +1,10 @@
+module.exports = {
+  capShape: [
+    {value: 0, label: "bell"},
+    {value: 2, label: "flat"},
+    {value: 1, label: "conical"},
+    {value: 3, label: "knobbed"},
+    {value: 4, label: "sunken"},
+    {value: 5, label: "convex"},
+  ]
+}
